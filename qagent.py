109806db023d1a86adf613ff6608a7602eb2d131
Ari Mirsky <arimirsky@gmail.com>
2022-10-28 11:42:51 -0400

    Initial result of qagent

---
+++ qagent.py
@@ -0,0 +1,54 @@
+import numpy as np
+from getPlayableActions import getPlayableActions
+
+
+class QAgent():
+    def getRandomState(self):
+        result = []
+        for i in range(len(self.q.shape)):
+            result.append(np.random.randint(0, high=np.max(self.q, axis=i)))
+        return result
+
+    def set_up_rewards(self, end_state):
+        rewards_new = np.copy(self.rewards)
+
+        # Set goal to be very high reward
+        rewards_new[end_state, end_state] = 999
+
+        return rewards_new
+
+    def qlearning(self, rewards_new, iterations):
+        for _ in range(iterations):
+            current_state = self.getRandomState()
+            playable_actions = getPlayableActions(
+                current_state, self.differentials, self.dt)
+            next_state = np.random.choice(playable_actions)
+            temporal_difference = rewards_new[current_state, next_state] + self.gamma * \
+                self.q[next_state, np.argmax(self.q[next_state, ])] - \
+                self.q[current_state, next_state]
+            self.q[current_state, next_state] += self.alpha * \
+                temporal_difference
+
+    def get_optimal_route(self, start_state, end_state):
+        route = [start_state]
+        next_state = start_state
+        while next_state != end_state:
+            next_state = np.argmax(self.q[start_state])
+            route.append(next_state)
+            start_state = next_state
+
+        return route
+
+    def training(self, start_state, end_state, iterations):
+        rewards_new = self.set_up_rewards(end_state)
+        self.qlearning(rewards_new, iterations)
+        route = self.get_optimal_route(start_state, end_state)
+        return route
+
+    def __init__(self, alpha, gamma, actions, rewards, dt):
+        self.gamma = gamma
+        self.alpha = alpha
+        self.actions = actions
+        self.rewards = rewards
+        self.q, self.differentials = getStateMatrix()
+        self.dt = dt
