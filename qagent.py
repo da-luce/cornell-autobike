788564f167a39075acd45033520fe8b335dd5fd0
Ari Mirsky <arimirsky@gmail.com>
2022-11-17 16:51:51 -0500

    Example testing

--- qagent.py
+++ qagent.py
@@ -1,12 +1,13 @@
 import numpy as np
-from HardToWriteFunctions import getPlayableActions, getStateMatrix
+#from HardToWriteFunctions import getPlayableActions, getStateMatrix
+from exampleqagent import getPlayableActions, getStateMatrix
 
 
 class QAgent():
     def getRandomState(self):
         result = []
         for i in range(len(self.q.shape)):
-            result.append(np.random.randint(0, high=np.max(self.q, axis=i)))
+            result.append(np.random.randint(0, high=self.q.shape[i]))
         return tuple(result)
 
     def set_up_rewards(self, end_state):
@@ -17,14 +18,15 @@ class QAgent():
 
         return rewards_new
 
-    def qlearning(self, rewards_new, iterations):
+    def qlearning(self, rewards_new, iterations, end_state):
         for _ in range(iterations):
             current_state = self.getRandomState()
+            if current_state == end_state:
+                continue
             playable_actions = getPlayableActions(
                 current_state, self.differentials, self.dt)
-            next_state = np.random.choice(playable_actions)
             temporal_difference = rewards_new[current_state] + self.gamma * \
-                self.q[np.argmax(self.q[playable_actions])] - \
+                np.amax(self.q[playable_actions]) - \
                 self.q[current_state]
             self.q[current_state] += self.alpha * \
                 temporal_difference
@@ -35,21 +37,25 @@ class QAgent():
         while next_state != end_state:
             playable_actions = getPlayableActions(
                 next_state, self.differentials, self.dt)
-            next_state = np.argmax(self.q[playable_actions])
+            t1 = self.q[playable_actions]
+            t2 = np.argmax(self.q[playable_actions])
+            t3 = playable_actions[0]
+            next_state = playable_actions[0][np.argmax(
+                self.q[playable_actions])]
             route.append(next_state)
 
         return route
 
     def training(self, start_state, end_state, iterations):
         rewards_new = self.set_up_rewards(end_state)
-        self.qlearning(rewards_new, iterations)
+        self.qlearning(rewards_new, iterations, end_state)
+        print(self.q.reshape((3, 3)))
         route = self.get_optimal_route(start_state, end_state)
         return route
 
-    def __init__(self, alpha, gamma, actions, rewards, dt):
+    def __init__(self, alpha, gamma, rewards, dt):
         self.gamma = gamma
         self.alpha = alpha
-        self.actions = actions
         self.rewards = rewards
         self.q, self.differentials = getStateMatrix()
         self.dt = dt
