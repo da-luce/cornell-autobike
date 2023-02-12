9c60f9faee053653d8537b23b75c3524a7e8fd0d
Jeanie <jc2885@cornell.edu>
2023-02-12 14:19:46 -0500

    added some more files, when running tox in terminal, dummy test case passes flake8 and mypy

---
+++ tests/testingfile.py
@@ -0,0 +1,12 @@
+"""dummy test case just to see if testing configurations work
+
+from qlearning import *"""
+
+
+def dummy_test():
+    assert 1 > 0
+
+
+"""if __name__ == "__main__":
+    dummy_test()
+    print("end") """
