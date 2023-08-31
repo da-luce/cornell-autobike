59f0a00828a7b7710fada6392a2904690247596e
Jeanie <jc2885@cornell.edu>
2023-08-31 15:31:38 -0400

    dummy test

--- tests/testingfile.py
+++ tests/testingfile.py
@@ -1,12 +1,28 @@
+<<<<<<< HEAD
 # dummy test case just to see if testing configurations work
 
 from qlearning import *
+=======
+#dummy test case just to see if testing configurations work
+
+#from qlearning import *
+>>>>>>> c2cc22c (dummy test)
 
 
 def dummy_test():
     assert 1 > 0
 
+def dummy_test2():
+    assert 1 != 0
+    
 
+<<<<<<< HEAD
 if __name__ == "__main__":
     dummy_test()
     print("end")
+=======
+
+if __name__ == "__main__":
+    dummy_test()
+
+>>>>>>> c2cc22c (dummy test)
