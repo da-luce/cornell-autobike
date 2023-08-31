52a85e0bae254eb49c848fab316a04dd4ea4867d
Jeanie <jc2885@cornell.edu>
2023-08-31 15:55:20 -0400

    dummy test

--- tests/testingfile.py
+++ tests/testingfile.py
@@ -6,7 +6,10 @@ from qlearning import *"""
 def dummy_test():
     assert 1 > 0
 
+def dummy_test2():
+    assert 2 != 0
 
-"""if __name__ == "__main__":
+
+if __name__ == "__main__":
     dummy_test()
-    print("end") """
+    dummy_test2()
