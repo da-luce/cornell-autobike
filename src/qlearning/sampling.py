d691ae76ac1ec8791e4d9f7d220981d646c3e6f1
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2023-09-28 15:40:46 -0400

    added plotting code

--- src/qlearning/sampling.py
+++ src/qlearning/sampling.py
@@ -1,6 +1,7 @@
 from qAgent import QAgent
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 from state_prediction import get_possible_states
 
 acceleration_power = 1  # m/s/s
@@ -8,6 +9,32 @@ braking_power = 1  # m/s/s
 max_turning_rate = 30  # deg/s
 
 #acc_res, turning_res, density of states that combo results in
+acc_res = 0
+turning_res = 0
 
-def isReachable(currentState, futureState, timestep):
+res = [acc_res, turning_res]
+
+#use diff values of res to check the output
+temp = get_possible_states(curr, diff, res)
+
+#use very high res to get close to 100% of states to be used as the benchmark 
+#for checking sampling density
+total_samples = get_possible_states(curr, diff, res)
+
+sample_size = len(temp)
+total_samples_size = len(total_samples)
+
+sample_density = (sample_size/total_samples_size)*100
+
+plt.figure()
+ax = plt.axes(projection="3d")
+ax.scatter(acc_res, turning_res, sample_density)
+
+#plt.plot(acc_res, turning_res, sample_density)
+
+ax.set_xlabel("acceleration res")
+ax.set_ylabel("turning res")
+ax.set_zlabel("sampling density")
+ax.view_init(60,35)
+plt.show()
 
