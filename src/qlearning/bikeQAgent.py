5eb98cc0508c1404580fd700851211b9bd5b7c5a
Ari Mirsky <arimirsky@gmail.com>
2023-10-05 16:29:39 -0400

    Still need to figure out how to import functions

--- src/qlearning/bikeQAgent.py
+++ src/qlearning/bikeQAgent.py
@@ -1,5 +1,9 @@
-from qAgent import QAgent
-import math
+from qagent import QAgent
+import sys
+
+sys.path.append("/usr/app/src")
+sys.path.append("/usr/app/src/state_pred")
+from state_pred import get_possible_states
 import numpy as np
 
 
@@ -13,59 +17,24 @@ class BikeQAgent(QAgent):
         speed, and angle. [differentials] is also 4 numbers, but is the
         differences between cells in the matrix in SI units. [timestep] is what states are
         possible after [timestep] amount of time."""
-        acceleration_power = 1  # m/s/s
-        braking_power = 1  # m/s/s
-        max_turning_rate = 30  # deg/s
 
-        # calculating max velocity reachable
-        max_vel = currentState[2] + acceleration_power*(timestep)
+        # To be replaced with issue #21
+        STEERING_SAMPLING_DENSITY = 0.03  # Radians
+        ACCELERATION_SAMPLING_DENSITY = 0.6  # m/s^2
 
-        # calculating min velocity reachable
-        min_vel = currentState[2] - braking_power*(timestep)
+        resolution = [ACCELERATION_SAMPLING_DENSITY, STEERING_SAMPLING_DENSITY]
 
-        # calculating max clockwise angle reachable
-        clock_max_angle = (currentState[3]-(max_turning_rate*timestep)) % 360
-
-        # calculating max counter-clockwise angle reachable
-        counter_max_angle = (currentState[3]+(max_turning_rate*timestep)) % 360
-
-        # min_x_coordinate
-        init_x_coord = currentState[0]
-
-        # min_y_coordinate
-        init_y_coord = currentState[1]
-
-        # calculating max displacement possible if continuing on same path with max acceleration
-        max_S = currentState[2]*timestep + 0.5 * \
-            acceleration_power*timestep*timestep
-
-        # calculating max change in x coordinate
-        max_x_coord = init_x_coord + (max_S * math.cos(currentState[3]))
-
-        # calculating max change in y coordinate
-        max_y_coord = init_y_coord + (max_S * math.sin(currentState[3]))
-
-        angle_diff_check = counter_max_angle - (clock_max_angle-360)
-
-        """while angle_diff_check/differentials[3] > 0:
-            x1 = clock_max_angle/differentials[3] +
-            return [[[(x, y, z, x1) for x in range(init_x_coord/differentials[0], max_x_coord/differentials[0])]
-            for y in range(init_y_coord/differentials[1], max_y_coord/differentials[1])]
-            for z in range(min_vel/differentials[2], max_vel/differentials[2])]
-
-            for x1 in range(clock_max_angle/differentials[3], counter_max_angle/differentials[3])]
-
-            angle_diff_check/differentials[3] -= 1"""
-        # Temporarily commented out while not compiling
-
-        raise NotImplementedError
+        return get_possible_states(currentState, differentials, resolution)
 
     def getStateMatrix(self):
         """Returns a tuple, the first element is a matrix with dimensions: x coordinate,
         y coordinate, speed, angle. The second element is the differences between
         each element of the matrix in SI units. This function should be determined before
-        compile-time based on the occupancy grid resolution and other physical factors."""
-        return np.zeros((1000, 1000, 10000, 10000, 360))
+        compile-time based on the occupancy grid resolution and other physical factors.
+        """
+        return np.zeros((100, 100, 10, 10, 10, 10)), np.array(
+            [0.2, 0.2, 0.2, 0.2, np.radians(0.01), np.radians(0.01)]
+        )
 
     def set_up_rewards(self, end_state):
         rewards_new = np.copy(self.rewards)
--- src/qlearning/main.py
+++ src/qlearning/main.py
@@ -1,7 +1,17 @@
 import smallExampleQAgent
+import bikeQAgent
 import numpy as np
 
 if __name__ == "__main__":
-    qagent = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((9,)))
-    route = qagent.training(0, 8, 100000)
-    print(route)
+    if False:
+        qagent = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((9,)))
+        route = qagent.training(0, 8, 100000)
+        print(route)
+    if True:
+        qagent = bikeQAgent.BikeQAgent(
+            0.1,
+            0.9,
+            np.zeros((100, 100, 10, 10, 10, 10)),
+        )
+        route = qagent.training(0, 8, 100000)
+        print(route)
