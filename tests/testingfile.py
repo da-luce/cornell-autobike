78221174e11504638d13fcca7d50c5ff7f661545
Cathy Miao <cm823@cornell.edu>
2023-10-12 16:27:24 -0400

    new way of importing

--- tests/testingfile.py
+++ tests/testingfile.py
@@ -9,18 +9,22 @@ def dummy_test():
     assert 1 > 0
 
 def q_matrix_test():
-    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((9,)))
-    print(qagent.QAgent.qlearning(bike_sample, np.zeros((9,)), 1000, 10))
+    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
+    qagent.QAgent.qlearning(bike_sample, np.zeros((10,)), 1000, 10)
+    print(bike_sample)
 
 def reset_matrix_test():
-    bike_sample = SmallExampleQAgent(0.1, 0.9, np.zeros((9,)))
-    print(QAgent.reset_matrix(bike_sample, np.zeros((9,)), 1000, 10))
+    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
+    qagent.QAgent.reset_matrix(bike_sample, np.zeros((10,)), 1000, 10)
+    print(bike_sample)
 
 def alter_matrix_test():
-    bike_sample = SmallExampleQAgent(0.1, 0.9, np.zeros((9,)))
-    print(QAgent.alter_matrix(bike_sample, np.zeros((9,)), 1000, 100))
+    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
+    qagent.QAgent.alter_matrix(bike_sample, np.zeros((10,)), 1000, 100)
+    print(bike_sample)
 
 if __name__ == "__main__":
     dummy_test()
     q_matrix_test()
+    alter_matrix_test()
     reset_matrix_test()
