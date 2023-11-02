03bedc8567612caf178404108bb51776667bba97
Cathy Miao <cm823@cornell.edu>
2023-11-02 15:31:39 -0400

    set up unit tests

--- tests/testingfile.py
+++ tests/testingfile.py
@@ -1,3 +1,4 @@
+import unittest
 import numpy as np
 import sys
 sys.path.append("/usr/app/src")
@@ -5,26 +6,53 @@ sys.path.append("/usr/app/src")
 from qlearning import qagent
 from qlearning import smallExampleQAgent
 
-def dummy_test():
-    assert 1 > 0
+class TestQAgent(unittest.TestCase):
+    def dummy_test():
+        assert 1 > 0
 
-def q_matrix_test():
-    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
-    qagent.QAgent.qlearning(bike_sample, np.zeros((10,)), 1000, 10)
-    print(bike_sample)
+    def setUp(self):
+        self.alpha = 0.1
+        self.gamma = 0.9
+        self.rewards = np.zeros((9,))
+        self.agent = smallExampleQAgent.SmallExampleQAgent(self.alpha, self.gamma, self.rewards)
 
-def reset_matrix_test():
-    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
-    qagent.QAgent.reset_matrix(bike_sample, np.zeros((10,)), 1000, 5, 1)
-    print(bike_sample)
+    def test_q_learning(self):
+        iterations = 1000
+        end_state = 8
+        self.agent.qlearning(self.rewards, iterations, end_state)
+    
+    def test_q_learning_2(self):
+        iterations = 0
+        end_state = 5
+        self.agent.qlearning(self.rewards, iterations, end_state)
+    
+    def test_reset_matrix(self):
+        iterations = 1000
+        end_state = 8
+        dimensions = 1
+        self.agent.reset_matrix(self.rewards, iterations, end_state, dimensions)
+        self.assertFalse(np.any(self.agent.q))
+    
+    def test_alter_matrix(self):
+        iterations = 1000
+        end_state = 8
+        scale = 0.5
+        self.agent.alter_matrix(self.rewards, iterations, end_state, scale)
+        self.assertFalse(np.any(self.agent.q))
+    
+"""
 
-def alter_matrix_test():
-    bike_sample = smallExampleQAgent.SmallExampleQAgent(0.1, 0.9, np.zeros((10,10)))
-    qagent.QAgent.alter_matrix(bike_sample, np.zeros((10,)), 1000, 10)
-    print(bike_sample)
+    def test_get_optimal_route(self):
+        start_state, end_state = 0, 8
+        route = self.agent.get_optimal_route(start_state, end_state)
+        self.assertEqual(route[-1], end_state)
+
+    def test_training(self):
+        start_state, end_state, iterations = 0, 8, 100
+        route = self.agent.training(start_state, end_state, iterations)
+        self.assertEqual(route[-1], end_state)
+"""
+
 
 if __name__ == "__main__":
-    dummy_test()
-    q_matrix_test()
-    reset_matrix_test()
-    alter_matrix_test()
+    unittest.main()
