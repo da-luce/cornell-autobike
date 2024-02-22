e9668ebc998d6ccab3cad1c0fc9a037389f45ee4
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2024-02-22 17:05:57 -0500

    changed loops for sampling from while to for

--- src/qlearning/sampling.py
+++ src/qlearning/sampling.py
@@ -27,7 +27,7 @@ turning_ult = 0.1
               #yaw_angle   (rad),
               #steer_angle (rad)])
 
-curr1 = np.array([0, 0, 5, 5, 0.5, 0.5])
+curr1 = np.array([10, 10, 5, 5, 2.1, 0.67])
 
 differentials = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 
@@ -44,76 +44,86 @@ total_samples_size = len(total_samples)
 acc_res_size = 1000
 turn_res_size = 1000
 
-densities_shape = (acc_res_size, turn_res_size)
-#densities = np.empty(densities_shape)
-densities = []
 #res_shape = (acc_res_size, turn_res_size) #500 rows for acc_res, 1000 columns for turn_res
 
-acc_res = []
-turn_res = []
+densities = np.zeros((acc_res_size, turn_res_size))
+acc_res = np.zeros(acc_res_size)
+turn_res = np.zeros(turn_res_size)
 
-#res = np.empty(res_shape)
+pos_i, pos_j = 0, 0
 
-pos_i = 0
-pos_j = 0
-i = 0.1
+for i in range(turn_res_size):
+  vari=0.1
+  for j in range(acc_res_size):
+    varj=0.01
+    acc_res[j] = varj
+    turn_res[i] = vari
 
-while (i<=100):
-  if i>0.1:
-    pos_i+=1
-  j=0.01
-  while (j<=10):
-    if j>0.01:
-      pos_j+=1
-    acc_res.append(j)
-    turn_res.append(i)
-    temp = (get_possible_states(curr1, differentials, (np.array([j, i]))))
+    temp = (get_possible_states(curr1, differentials, (np.array([varj, vari]))))
     sample_size = len(temp)
-    #print(sample_size)
     sample_density = (sample_size/total_samples_size)*100
-    #print(sample_density)
-    #densities[pos_i][pos_j] = sample_density
-    densities.append(sample_density)
-    j+=0.01
+    densities[pos_i, pos_j] = sample_density
 
-  i+=0.1
+    varj+=0.01
 
-final_densities = np.array(densities)
-acc_res = np.array(acc_res)
-turn_res = np.array(turn_res)
+  vari+=0.1
+
+#print(densities.shape)
 
 # print(len(acc_res))
 # print(len(turn_res))
-# final_len = len(final_densities)
-# print(final_len)
+# final_len = len(densities)
 
-
-# densities_pos=0
-# for x in res:
-#   temp = (get_possible_states(curr1, differentials, x))
-#   sample_size = len(temp)
-#   densities[densities_pos] = sample-density
-#   densities_pos+=1
-
-#for i in final_densities:
-  #print(i)
-
-#use diff values of res to check the output
-#temp = get_possible_states(curr, diff, res)
-
-
-#sample_density = (sample_size/total_samples_size)*100
 
 plt.figure()
 ax = plt.axes(projection="3d")
-ax.scatter(acc_res, turn_res, final_densities)
+ax.scatter(acc_res, turn_res, densities)
 
-plt.plot(acc_res, turn_res, final_densities)
+plt.plot(acc_res, turn_res, densities)
 
 ax.set_xlabel("acceleration res")
 ax.set_ylabel("turning res")
 ax.set_zlabel("sampling density")
 ax.view_init(60,35)
 
-plt.savefig('sampling_plot.png')
+plt.savefig('sampling_plot5.png')
+
+
 #plt.show()
+
+
+
+
+
+
+
+# acc_res = []
+# turn_res = []
+
+# pos_i = 0
+# pos_j = 0
+
+# i = 0.1
+# while (i<=100):
+#   if i>0.1:
+#     pos_i+=1
+#   j=0.01
+#   while (j<=10):
+#     if j>0.01:
+#       pos_j+=1
+#     acc_res.append(j)
+#     turn_res.append(i)
+#     temp = (get_possible_states(curr1, differentials, (np.array([j, i]))))
+#     sample_size = len(temp)
+#     #print(sample_size)
+#     sample_density = (sample_size/total_samples_size)*100
+#     #print(sample_density)
+#     densities[pos_i, pos_j] = sample_density
+#     #densities.append(sample_density)
+#     j+=0.01
+
+#   i+=0.1
+
+
+# final_densities = np.array(densities).reshape(len(acc_res), -1)
+# print(final_densities.shape)
