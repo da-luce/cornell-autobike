dfca2a266177c74345f7e9b2eccb8e700535d000
Jeanie <jc2885@cornell.edu>
2024-04-18 17:03:43 -0400

    making qagent objects for the main loop

--- main_loop/check_waypoint.py
+++ main_loop/check_waypoint.py
@@ -5,17 +5,15 @@ import qlearning/qagent.py
 
 
 #check how the data is stored as
-def check_waypoint(curr_state, vision, curr_waypoint, next_waypoint):
+def check_waypoint(curr_state, curr_waypoint, rewards, old_qagent):
 	x_s = curr_state[0]
 	y_s = curr_state[1]
 	x_w = curr_waypoint[0]
 	y_w = curr_waypoint[1]
-	x_n = next_waypoint[0]
-	y_n = next_waypoint[1]
 	
 	point1 = np.array((x_s, y_s))
         point2 = np.array((x_w, y_w))
-        point3 = np.array((x_n, y_n))
+
         dist = np.linalg.norm(point1 - point2)
         
 	tolerance = 0.5 #to change
@@ -25,6 +23,7 @@ def check_waypoint(curr_state, vision, curr_waypoint, next_waypoint):
 	
 
 	if (dist <= tolerance):
-                next_dist = np.linalg.norm(point1 - point3)
-                rewards = get_rewards(vision, next_dist) 
                 qAgent = BikeQAgent(alpha, gamma, rewards, dt)
+                return qAgent
+        else:
+                return old_qagent
---
+++ main_loop/main.py
@@ -0,0 +1,83 @@
+from pykalman import KalmanFilter
+import numpy as np
+import qlearning/bikeQAgent.py
+import qlearning/qagent.py
+import main_loop/check_waypoint.py
+
+def main():
+    # Initialize state
+    current_state = [0, 0, 0, 0, 0, 0, 0]
+
+    while(True):
+        #get the waypoints somehow...
+        curr_waypoint = []
+        next_waypoint = []
+        
+        # Receiving data from GPS
+        gps = get_gps()
+
+        # Receiving data from vision
+        vision = get_vision()
+
+        #initialize qagent
+        alpha = 0
+        gamma = 0
+
+	curr = np.array((curr_state[0], curr_state[1]))
+        next_pt = np.array((next_waypoint[0], next_waypoint[1]))
+        next_dist = np.linalg.norm(curr - next_pt)
+        rewards = get_rewards(vision, next_dist)
+        
+        dt = 0
+        qagent = BikeQAgent(alpha, gamma, rewards, dt)
+        
+        # Update state given GPS data
+        current_state = update_state(gps)
+
+        #check waypoint, return original or new qagent
+        qagent = check_waypoint(current_state, curr_waypoint, rewards, qagent)
+
+        #run qlearning
+        iterations = 1000
+        scale = 0.9
+        qagent.alter_matrix(rewards, iterations, next_waypoint, scale)
+        # Find optimal steering angle
+        steering_angle = get_steering_angle(current_state)
+
+        # Send data to controls
+        output_data(steering_angle)
+
+        # Put an end state
+
+        # if end_state is reached:
+        #     break
+
+def calculate_moving_average(state, data):
+    # take average of current state and gps data
+
+    return
+
+# Receives data from GPS
+def get_gps():
+    return
+
+# Receives data from vision
+def get_vision():
+    return
+
+# Updates state given GPS data
+def update_state(state, gps):
+    return state
+
+# Returns optimal steering angle
+def get_steering_angle(state):
+    steering_angle = 0.0
+
+    return steering_angle
+
+# Sends steering angle data to controls
+def output_data(steering_angle):
+    return
+
+if __name__ == "__main__":
+    main()
