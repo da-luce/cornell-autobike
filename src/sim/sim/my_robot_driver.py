5eca60175724a9979dc867daa9eb5753bea156dd
Firdavs <fdy3@cornell.edu>
2025-03-03 17:19:04 -0500

    Added a gyroscope to the bike simulation along with making our node an imu publisher

--- src/sim/sim/my_robot_driver.py
+++ src/sim/sim/my_robot_driver.py
@@ -1,7 +1,7 @@
 import numpy as np
 import rclpy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan, NavSatFix, PointCloud2, PointField
+from sensor_msgs.msg import Imu, LaserScan, NavSatFix, PointCloud2, PointField
 from std_msgs.msg import Float32
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
@@ -32,11 +32,32 @@ class MyRobotDriver:
 
         # PointCloud2 Publisher
         self.__pointcloud_publisher = self.__node.create_publisher(PointCloud2, '/pointcloud', 10)
+        # GPS publisher
         self.__gps_publisher = self.__node.create_publisher(NavSatFix, '/gps', 10)
+        # Gyroscope publisher
+        self.__gyro_publisher = self.__node.create_publisher(Imu, '/gyro', 10)
 
         # Initialize GPS
         self.__gps = self.__robot.getDevice("gps")
         self.__gps.enable(32)
+
+        #Initialize the Gyroscope
+        self.__gyro = self.__robot.getDevice("gyro")
+        self.__gyro.enable(32)
+
+    def __gyro_callback(self):
+        gyro_vals = self.__gyro.getValues()
+        imu = Imu()
+
+        imu.angular_velocity.x = gyro_vals[0]
+        imu.angular_velocity.y = gyro_vals[1]
+        imu.angular_velocity.z = gyro_vals[2]
+        self.__gyro_publisher.publish(imu)
+        self.__node.get_logger().info('Publishing Gyroscope Data: X: %.2f, Y: %.2f, Z: %.2f' %
+                               (imu.angular_velocity.x,
+                                imu.angular_velocity.y,
+                                imu.angular_velocity.z))
+
 
     def __angle_callback(self, msg):
         """ Callback function to handle received angle data """
@@ -58,7 +79,7 @@ class MyRobotDriver:
 
     def __gps_callback(self):
         """ Callback function to publish GPS data """
-        gps_value = self.__gps.getValues()  # Corrected to self.__gps
+        gps_value = self.__gps.getValues()
 
         # Create a NavSatFix message and populate it with GPS data
         gps_msg = NavSatFix()
@@ -120,6 +141,9 @@ class MyRobotDriver:
         self.__node.get_logger().info(f"Stepping!")
         self.__node.get_logger().info("Publishing GPS data...")
         self.__gps_callback()
+
+        self.__node.get_logger().info("Publishing Gyroscope data...")
+        self.__gyro_callback()
 
         forward_speed = self.__target_twist.linear.x
         angular_speed = self.__target_twist.angular.z
