3443afdcb169629b70f5e435e4b3695f2b62dfcf
Si Un Kim <Anniesiun@gmail.com>
2025-03-11 16:41:07 -0400

    parameters for lidar & grid changed

--- src/sim/launch/robot_launch.py
+++ src/sim/launch/robot_launch.py
@@ -48,13 +48,13 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {'cloud_in_topic': '/pointcloud'},  # Set your topic here
-            {'position_x': -5.0},
+            {'position_x': 0.0},
             {'position_y': 0.0},
             {'verbose1': False},
             {'verbose2': False},
-            {'cell_size': 0.5},
-            {'length_x': 50.0},
-            {'length_y': 100.0},
+            {'cell_size': 0.1},
+            {'length_x': 10.0},
+            {'length_y': 10.0},
             {'mapi_topic_name': 'intensity_grid'},
             {'maph_topic_name': 'height_grid'},
         ]
